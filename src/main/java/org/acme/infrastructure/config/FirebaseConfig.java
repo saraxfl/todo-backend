754fd0145a95f74f3aa1a0bd73e08fb6bff74101
saraxfl <A07045944@tec.mx>
2026-06-01 15:30:54 -0600

    Use Firebase env variable

--- src/main/java/org/acme/infrastructure/config/FirebaseConfig.java
+++ src/main/java/org/acme/infrastructure/config/FirebaseConfig.java
@@ -7,33 +7,55 @@ import jakarta.annotation.PostConstruct;
 import jakarta.inject.Singleton;
 import io.quarkus.runtime.Startup;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Singleton
 @Startup
 public class FirebaseConfig {
 
-
     @PostConstruct
     void init() {
         try {
+
             if (FirebaseApp.getApps().isEmpty()) {
-                String credentialsPath = System.getenv("FIREBASE_CREDENTIALS_PATH");
 
-                InputStream serviceAccount = (credentialsPath != null)
-                        ? new java.io.FileInputStream(credentialsPath)
-                        : getClass().getClassLoader()
-                        .getResourceAsStream("firebase-service-account.json");
+                String json = System.getenv(
+                        "FIREBASE_SERVICE_ACCOUNT_JSON"
+                );
 
-                assert serviceAccount != null;
-                FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                if (json == null || json.isBlank()) {
+                    throw new RuntimeException(
+                            "Falta FIREBASE_SERVICE_ACCOUNT_JSON"
+                    );
+                }
+
+                InputStream serviceAccount =
+                        new ByteArrayInputStream(
+                                json.getBytes(
+                                        StandardCharsets.UTF_8
+                                )
+                        );
+
+                FirebaseOptions options =
+                        FirebaseOptions.builder()
+                        .setCredentials(
+                                GoogleCredentials.fromStream(
+                                        serviceAccount
+                                )
+                        )
                         .build();
 
                 FirebaseApp.initializeApp(options);
             }
+
         } catch (Exception e) {
-            throw new RuntimeException("Error inicializando Firebase", e);
+
+            throw new RuntimeException(
+                    "Error inicializando Firebase",
+                    e
+            );
         }
     }
 }
